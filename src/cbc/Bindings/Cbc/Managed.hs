7f40a09da759298e04e594c2f157fa4aa236d36c
natebragg <nate@cs.tufts.edu>
2020-05-30 11:23:25 -0400

    Use newtype in managed interfaces instead of type alias.

--- src/cbc/Bindings/Cbc/Managed.hs
+++ src/cbc/Bindings/Cbc/Managed.hs
@@ -43,91 +43,91 @@ import Foreign.C.String (CString)
 import Foreign.C.Types (CDouble(..), CInt(..))
 import Foreign.ForeignPtr (ForeignPtr, newForeignPtr, withForeignPtr)
 
-type ModelHandle = ForeignPtr Unmanaged.Model
+newtype ModelHandle = ModelHandle { runModel :: ForeignPtr Unmanaged.Model }
 
 newModel :: IO ModelHandle
-newModel = Unmanaged.newModel >>= newForeignPtr Unmanaged.deleteModel
+newModel = Unmanaged.newModel >>= newForeignPtr Unmanaged.deleteModel >>= pure . ModelHandle
 
 readMps :: ModelHandle -> CString -> Bool -> Bool -> IO CInt
-readMps model filename keepNames ignoreErrors = withForeignPtr model $ \model ->
+readMps model filename keepNames ignoreErrors = withForeignPtr (runModel model) $ \model ->
     Unmanaged.readMps model filename keepNames ignoreErrors
 
 getObjSense :: ModelHandle -> IO CDouble
-getObjSense model = withForeignPtr model $ \model ->
+getObjSense model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getObjSense model
 
 setObjSense :: ModelHandle -> CDouble -> IO ()
-setObjSense model value = withForeignPtr model $ \model ->
+setObjSense model value = withForeignPtr (runModel model) $ \model ->
     Unmanaged.setObjSense model value
 
 getRowLower :: ModelHandle -> IO (Ptr CDouble)
-getRowLower model = withForeignPtr model $ \model ->
+getRowLower model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getRowLower model
 
 getRowUpper :: ModelHandle -> IO (Ptr CDouble)
-getRowUpper model = withForeignPtr model $ \model ->
+getRowUpper model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getRowUpper model
 
 getObjCoefficients :: ModelHandle -> IO (Ptr CDouble)
-getObjCoefficients model = withForeignPtr model $ \model ->
+getObjCoefficients model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getObjCoefficients model
 
 getColLower :: ModelHandle -> IO (Ptr CDouble)
-getColLower model = withForeignPtr model $ \model ->
+getColLower model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getColLower model
 
 getColUpper :: ModelHandle -> IO (Ptr CDouble)
-getColUpper model = withForeignPtr model $ \model ->
+getColUpper model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getColUpper model
 
 getNumElements :: ModelHandle -> IO CInt
-getNumElements model = withForeignPtr model $ \model ->
+getNumElements model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getNumElements model
 
 getIndices :: ModelHandle -> IO (Ptr CInt)
-getIndices model = withForeignPtr model $ \model ->
+getIndices model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getIndices model
 
 getElements :: ModelHandle -> IO (Ptr CDouble)
-getElements model = withForeignPtr model $ \model ->
+getElements model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getElements model
 
 getObjValue :: ModelHandle -> IO CDouble
-getObjValue model = withForeignPtr model $ \model ->
+getObjValue model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getObjValue model
 
 solve :: ModelHandle -> IO CInt
-solve model = withForeignPtr model $ \model ->
+solve model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.solve model
 
 getNumRows :: ModelHandle -> IO CInt
-getNumRows model = withForeignPtr model $ \model ->
+getNumRows model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getNumRows model
 
 getNumCols :: ModelHandle -> IO CInt
-getNumCols model = withForeignPtr model $ \model ->
+getNumCols model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getNumCols model
 
 isAbandoned :: ModelHandle -> IO Bool
-isAbandoned model = withForeignPtr model $ \model ->
+isAbandoned model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isAbandoned model
 
 isProvenOptimal :: ModelHandle -> IO Bool
-isProvenOptimal model = withForeignPtr model $ \model ->
+isProvenOptimal model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isProvenOptimal model
 
 isProvenInfeasible :: ModelHandle -> IO Bool
-isProvenInfeasible model = withForeignPtr model $ \model ->
+isProvenInfeasible model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isProvenInfeasible model
 
 isSolutionLimitReached :: ModelHandle -> IO Bool
-isSolutionLimitReached model = withForeignPtr model $ \model ->
+isSolutionLimitReached model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isSolutionLimitReached model
 
 getRowActivity :: ModelHandle -> IO (Ptr CDouble)
-getRowActivity model = withForeignPtr model $ \model ->
+getRowActivity model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getRowActivity model
 
 getColSolution :: ModelHandle -> IO (Ptr CDouble)
-getColSolution model = withForeignPtr model $ \model ->
+getColSolution model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getColSolution model
--- src/clp/Bindings/Clp/Managed.hs
+++ src/clp/Bindings/Clp/Managed.hs
@@ -57,123 +57,123 @@ import Foreign.C.String (CString)
 import Foreign.C.Types (CDouble(..), CInt(..))
 import Foreign.ForeignPtr (ForeignPtr, newForeignPtr, withForeignPtr)
 
-type SimplexHandle = ForeignPtr Unmanaged.Simplex
+newtype SimplexHandle = SimplexHandle { runModel :: ForeignPtr Unmanaged.Simplex }
 
 newModel :: IO SimplexHandle
-newModel = Unmanaged.newModel >>= newForeignPtr Unmanaged.deleteModel
+newModel = Unmanaged.newModel >>= newForeignPtr Unmanaged.deleteModel >>= pure . SimplexHandle
 
 readMps :: SimplexHandle -> CString -> Bool -> Bool -> IO CInt
-readMps model filename keepNames ignoreErrors = withForeignPtr model $ \model ->
+readMps model filename keepNames ignoreErrors = withForeignPtr (runModel model) $ \model ->
     Unmanaged.readMps model filename keepNames ignoreErrors
 
 addRows :: SimplexHandle -> CInt -> Ptr CDouble -> Ptr CDouble -> Ptr CInt -> Ptr CInt -> Ptr CDouble -> IO ()
-addRows model number rowLower rowUpper rowStarts columns elements = withForeignPtr model $ \model ->
+addRows model number rowLower rowUpper rowStarts columns elements = withForeignPtr (runModel model) $ \model ->
     Unmanaged.addRows model number rowLower rowUpper rowStarts columns elements
 
 addColumns :: SimplexHandle -> CInt -> Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> Ptr CInt -> Ptr CInt -> Ptr CDouble -> IO ()
-addColumns model number columnLower columnUpper objective columnStarts rows elements = withForeignPtr model $ \model ->
+addColumns model number columnLower columnUpper objective columnStarts rows elements = withForeignPtr (runModel model) $ \model ->
     Unmanaged.addColumns model number columnLower columnUpper objective columnStarts rows elements
 
 getObjSense :: SimplexHandle -> IO CDouble
-getObjSense model = withForeignPtr model $ \model ->
+getObjSense model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getObjSense model
 
 setObjSense :: SimplexHandle -> CDouble -> IO ()
-setObjSense model value = withForeignPtr model $ \model ->
+setObjSense model value = withForeignPtr (runModel model) $ \model ->
     Unmanaged.setObjSense model value
 
 getRowLower :: SimplexHandle -> IO (Ptr CDouble)
-getRowLower model = withForeignPtr model $ \model ->
+getRowLower model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getRowLower model
 
 getRowUpper :: SimplexHandle -> IO (Ptr CDouble)
-getRowUpper model = withForeignPtr model $ \model ->
+getRowUpper model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getRowUpper model
 
 getObjCoefficients :: SimplexHandle -> IO (Ptr CDouble)
-getObjCoefficients model = withForeignPtr model $ \model ->
+getObjCoefficients model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getObjCoefficients model
 
 getColLower :: SimplexHandle -> IO (Ptr CDouble)
-getColLower model = withForeignPtr model $ \model ->
+getColLower model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getColLower model
 
 getColUpper :: SimplexHandle -> IO (Ptr CDouble)
-getColUpper model = withForeignPtr model $ \model ->
+getColUpper model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getColUpper model
 
 getNumElements :: SimplexHandle -> IO CInt
-getNumElements model = withForeignPtr model $ \model ->
+getNumElements model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getNumElements model
 
 getIndices :: SimplexHandle -> IO (Ptr CInt)
-getIndices model = withForeignPtr model $ \model ->
+getIndices model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getIndices model
 
 getVectorLengths :: SimplexHandle -> IO (Ptr CInt)
-getVectorLengths model = withForeignPtr model $ \model ->
+getVectorLengths model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getVectorLengths model
 
 getElements :: SimplexHandle -> IO (Ptr CDouble)
-getElements model = withForeignPtr model $ \model ->
+getElements model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getElements model
 
 getObjValue :: SimplexHandle -> IO CDouble
-getObjValue model = withForeignPtr model $ \model ->
+getObjValue model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getObjValue model
 
 setLogLevel :: SimplexHandle -> CInt -> IO ()
-setLogLevel model value = withForeignPtr model $ \model ->
+setLogLevel model value = withForeignPtr (runModel model) $ \model ->
     Unmanaged.setLogLevel model value
 
 initialSolve :: SimplexHandle -> IO CInt
-initialSolve model = withForeignPtr model $ \model ->
+initialSolve model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.initialSolve model
 
 dual :: SimplexHandle -> CInt -> IO CInt
-dual model pass = withForeignPtr model $ \model ->
+dual model pass = withForeignPtr (runModel model) $ \model ->
     Unmanaged.dual model pass
 
 getNumRows :: SimplexHandle -> IO CInt
-getNumRows model = withForeignPtr model $ \model ->
+getNumRows model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getNumRows model
 
 getNumCols :: SimplexHandle -> IO CInt
-getNumCols model = withForeignPtr model $ \model ->
+getNumCols model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getNumCols model
 
 isAbandoned :: SimplexHandle -> IO Bool
-isAbandoned model = withForeignPtr model $ \model ->
+isAbandoned model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isAbandoned model
 
 isProvenOptimal :: SimplexHandle -> IO Bool
-isProvenOptimal model = withForeignPtr model $ \model ->
+isProvenOptimal model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isProvenOptimal model
 
 isProvenPrimalInfeasible :: SimplexHandle -> IO Bool
-isProvenPrimalInfeasible model = withForeignPtr model $ \model ->
+isProvenPrimalInfeasible model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isProvenPrimalInfeasible model
 
 isProvenDualInfeasible :: SimplexHandle -> IO Bool
-isProvenDualInfeasible model = withForeignPtr model $ \model ->
+isProvenDualInfeasible model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isProvenDualInfeasible model
 
 isPrimalObjectiveLimitReached :: SimplexHandle -> IO Bool
-isPrimalObjectiveLimitReached model = withForeignPtr model $ \model ->
+isPrimalObjectiveLimitReached model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isPrimalObjectiveLimitReached model
 
 isDualObjectiveLimitReached :: SimplexHandle -> IO Bool
-isDualObjectiveLimitReached model = withForeignPtr model $ \model ->
+isDualObjectiveLimitReached model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isDualObjectiveLimitReached model
 
 isIterationLimitReached :: SimplexHandle -> IO Bool
-isIterationLimitReached model = withForeignPtr model $ \model ->
+isIterationLimitReached model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.isIterationLimitReached model
 
 getRowActivity :: SimplexHandle -> IO (Ptr CDouble)
-getRowActivity model = withForeignPtr model $ \model ->
+getRowActivity model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getRowActivity model
 
 getColSolution :: SimplexHandle -> IO (Ptr CDouble)
-getColSolution model = withForeignPtr model $ \model ->
+getColSolution model = withForeignPtr (runModel model) $ \model ->
     Unmanaged.getColSolution model
